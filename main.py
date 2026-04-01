346cc43d41af78e74e37cca40dab7b3a501929cf
Satvika35 <smallar2@student.gitam.edu>
2026-04-01 08:58:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 from predictor import predict_questions
 
 def main():
-    print("📚 Question Paper Predictor System\n")
+    print(" Question Paper Predictor System\n")
     
     predicted_questions = predict_questions()
     
-    print("🔥 Predicted Important Questions:\n")
+    print(" Predicted Important Questions:\n")
     
     for i, (question, count) in enumerate(predicted_questions, start=1):
         print(f"{i}. {question} (Repeated {count} times)")
