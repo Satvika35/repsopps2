6527430eb8ee2d732196d7abd6a082101a1258b2
Satvika35 <smallar2@student.gitam.edu>
2026-04-10 15:19:47 +0530

    Add main block for testing frequency functions
    Added a main block to execute sample frequency counting and top questions retrieval.

--- utils.py
+++ utils.py
@@ -10,3 +10,15 @@ def count_frequency(questions):
 def get_top_questions(frequency, top_n=3):
     # Get most common questions
     return frequency.most_common(top_n)
+
+if __name__ == "__main__":
+    sample_questions = [
+        "A", "B", "A", "C", "B", "A"
+    ]
+    
+    freq = count_frequency(sample_questions)
+    top = get_top_questions(freq)
+    
+    print("Frequency:", freq)
+    print("Top Questions:", top)
+
